88834d04980346940a7b3b3265b8eddf233bf388
Hansiyuan131 <422295068@qq.com>
2024-05-29 23:57:44 +0800

    feat: init

---
+++ src/main/java/com/yuanstack/xgateway/XGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yuanstack.xgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuanstack/xgateway/XGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuanstack.xgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
